095aa1bede3107f841390ac3a76615853034a087
grantadiele@gmail.com <grantobioma@gmail.com>
2024-07-29 20:21:10 +0100

    Update from vue to nuxt.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
 export default {
   content: [
     "./components/**/*.{js,vue,ts}",
@@ -26,16 +25,3 @@ export default {
   plugins: [],
 }
 
-=======
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      backgroundColor: {
-        'off-black': '#050505'
-      }
-    }
-  },
-  plugins: [],
-}
->>>>>>> d7226d6d6ceb394a1eafef6b3911d449bf9ad269
